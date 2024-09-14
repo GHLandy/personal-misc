c306348f3e84f63c256b761050f9206f856ce050
GHLandy <liftland953@163.com>
2024-09-14 17:20:47 +0800

    update

--- src/utils/listUnique.js
+++ src/utils/listUnique.js
@@ -1,10 +1,10 @@
 /**
  * 列表去重
  * @param {number[] | string[] | object[]} list 列表 (数字、字符串、对象列表)
- * @param {string} [uniqueKey] 通过那个键去重 (数字、字符串列表则不用传)
- * @param {boolean} [head=false] 默认 false 取后边的，true 取前边的
+ * @param {string} [key = null] 通过那个键去重 (数字、字符串列表则不用传)
+ * @param {boolean} [head = false] 默认 false 取后边的，true 取前边的
  */
-export function listDeWeight(list, uniqueKey, head = false) {
+export function listUnique(list, key = null, head = false) {
   /**
    * @type {any[]}
    */
@@ -16,8 +16,8 @@ export function listDeWeight(list, uniqueKey, head = false) {
     const leftArr = arr.slice(index + 1);
 
     let bool = leftArr.find((it) => it === item);
-    if (uniqueKey) {
-      bool = leftArr.find((it) => it[uniqueKey] === item[uniqueKey]);
+    if (key) {
+      bool = leftArr.find((it) => it[key] === item[key]);
     }
 
     if (bool) {
