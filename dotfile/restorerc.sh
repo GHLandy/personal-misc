8c8a9ee43cb575bba5a4e1580595359d103f8cee
GHLandy <liftland953@163.com>
2025-01-19 10:07:13 +0800

    update

--- dotfile/restorerc.sh
+++ dotfile/restorerc.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
+RC_DIR=$(dirname $0)
+
 RC_FILES=(
   ".envrc"
   ".gitconfig"
   ".npmrc"
 )
 
-RC_DIR=$(dirname $0)
-
 for rc in ${RC_FILES[*]}; do
   /usr/bin/cp -f $RC_DIR/$rc ~
   echo "restore $rc to ~/$rc"
 done
+
+if [[ $MSYSTEM ]]; then
+  BASH_FILES=(
+    ".bash_profile"
+    ".bashrc"
+  )
+
+  for rc in ${BASH_FILES[*]}; do
+    /usr/bin/cp -f $RC_DIR/$rc ~
+    echo "restore $rc to ~/$rc"
+  done
+fi
