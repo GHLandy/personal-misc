005772a3b9c7cbc9f84202a812f22c1b4f9b506a
GHLandy <liftland953@163.com>
2025-04-22 16:17:43 +0800

    eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+]);
