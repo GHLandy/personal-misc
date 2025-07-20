8b32ed2e8603715a9193cb307eb66098c357db3a
GHLandy <liftland953@163.com>
2025-07-20 21:31:52 +0800

    update

--- ts/index.ts
+++
@@ -1,5 +0,0 @@
-export const version = '0.1.0';
-
-export * from './ChunkUploader';
-export * from './Logger';
-export * from './WebsocketManager';
--- ts/randomHexStr.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 随机最多 15 位十六进制字符串
- */
-export function randomHexStr(): string {
-  return Number(Math.random().toFixed(15).slice(2)).toString(16);
-}
