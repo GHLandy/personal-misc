da1ec7a7a51e59811013f9e8213e516aedf5e555
GHLandy <liftland953@163.com>
2025-09-02 11:34:27 +0800

    update

---
+++ ps1/env.ps1
@@ -0,0 +1,9 @@
+# User、Machine
+
+[System.Environment]::SetEnvironmentVariable("FNM_NODE_DIST_MIRROR", "https://npmmirror.com/mirrors/node", "User")
+[System.Environment]::SetEnvironmentVariable("ELECTRON_MIRROR", "https://npmmirror.com/mirrors/electron/", "User")
+
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", "D:\Tools\jdk17.0.16_8", "User")
+
+[System.Environment]::SetEnvironmentVariable("GRADLE_HOME", "D:\Tools\gradle-8.10.2", "User")
+[System.Environment]::SetEnvironmentVariable("GRADLE_USER_HOME", "D:\.gradle", "User")
